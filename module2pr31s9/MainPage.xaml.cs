bb79ea65b1f6226dcee502eb0c7d0520575366b5
310-03 <310-03@310-03>
2021-12-08 11:55:18 +0500

    Добавьте файлы проекта.

---
+++ module2pr31s9/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace module2pr31s9
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+        public async void Next(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+    }
+}
